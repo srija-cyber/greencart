aa89fed3d3b97886a73519db5926f1f76a3265fa
srija-cyber <dhanwadasrija@gmail.com>
2025-08-13 08:32:57 +0530

    Render edit commit

--- backend/config.js
+++ backend/config.js
@@ -2,8 +2,6 @@ export default {
   PORT: process.env.PORT || 5000,
   // MONGODB_URI:'mongodb://localhost:27017/greencart',
   MONGODB_URI: 'mongodb+srv://dhanwadasrija:daQtOEfAkRevx9bh@cluster0.mscrjm8.mongodb.net/greencart?retryWrites=true&w=majority',
-
-
   JWT_SECRET: process.env.JWT_SECRET || 'greencart-super-secret-jwt-key-2024',
   NODE_ENV: process.env.NODE_ENV || 'production'
 };
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-const API_BASE_URL = '/api';
+// const API_BASE_URL = '/api';
+const API_BASE_URL =
+  window.location.hostname === 'localhost'
+    ? '/api'
+    : 'https://greencartlogistics.onrender.com/api';
 
 // Create axios instance with base configuration
 const api = axios.create({
